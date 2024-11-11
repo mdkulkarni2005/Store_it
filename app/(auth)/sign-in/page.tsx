0b0d68881dc8e36b776ef753c9fc8c933df4efcf
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2024-11-11 18:06:11 +0530

    auth done

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,9 +1,5 @@
-import React from 'react'
+import AuthForm from '@/components/AuthForm'
 
-const SignIn = () => {
-  return (
-    <div>SignIn page</div>
-  )
-}
+const SignIn = () => <AuthForm type="sign-in" />
 
 export default SignIn
